9c03ed023800591a6d4768c8d6c6fc6877c32746
wangyuxin11 <happiness20@163.com>
2019-09-20 00:06:16 +0800

    g

---
+++ GpBond-common/src/main/java/com/gpbond/stock/common/model/Stock.java
@@ -0,0 +1,40 @@
+package com.gpbond.stock.common.model;
+
+import java.math.BigDecimal;
+
+
+public class Stock {
+	
+	
+	public int code;  //代码
+	
+	public String name; //名称
+	
+	
+	public BigDecimal priceNow;   //当前价
+	
+	public BigDecimal priceLow;   //最低价
+	
+	public BigDecimal priceHigh;  //最高价
+	
+	public BigDecimal priceOpen;  //开盘价
+	
+	public BigDecimal priceClose; //昨收价
+	
+	public BigDecimal priceDiff;  //差价（= priceNow - priceOpen）
+	
+	 
+	public Long dealQuantity; //成交量
+	
+	public BigDecimal dealPriceTotal;  //成交额
+	
+	
+	public int turnoverRate; //换手率
+	
+	public int amplitude; //振幅
+	
+	
+	
+	
+
+}
--- GpBond-spider/src/main/java/com/gpbond/stock/spider/SpiderApplication.java
+++ GpBond-spider/src/main/java/com/gpbond/stock/spider/SpiderApplication.java
@@ -1,5 +1,15 @@
 package com.gpbond.stock.spider;
 
 public class SpiderApplication {
+	
+	
+	//java实现股票的最大利润(一次卖出，多次卖出）--中金所笔试题
+	//https://blog.csdn.net/qiannuo_/article/details/84062976
+	
+	
+	
+	
+	 //基于Java语言开发的个性化股票分析技术：随机指数(KDJ)
+	//https://www.docin.com/p-531063704.html
 
 }
